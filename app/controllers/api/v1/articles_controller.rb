e421c935219064055cba3e9385d0f6a506c2ca0d
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-10-30 10:31:15 +0700

    feat(linting): setup rubocop

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,55 +1,60 @@
 # frozen_string_literal: true
 
-class ArticlesController < ApplicationController
-  def index
-    @articles = @service.findAllArticle
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        @articles = @service.findAllArticle
 
-    render json: {
-      message: "success",
-      data: @articles
-    }, status: :ok
-  end
+        render json: {
+          message: 'success',
+          data: @articles
+        }, status: :ok
+      end
 
-  def show
-    @article = @service.findArticleById(params[:id])
+      def show
+        @article = @service.findArticleById(params[:id])
 
-    render json: {
-      message: "success",
-      data: @article
-    }, status: :ok
-  end
+        render json: {
+          message: 'success',
+          data: @article
+        }, status: :ok
+      end
 
-  def create
-    @article = @service.createArticle(article_params)
+      def create
+        @article = @service.createArticle(article_params)
 
-    render json: {
-      message: "success",
-      data: @article
-    }, status: :created
-  end
+        render json: {
+          message: 'success',
+          data: @article
+        }, status: :created
+      end
 
-  def update
-    @article = @service.updateArticle(params[:id], article_params)
+      def update
+        @article = @service.updateArticle(params[:id], article_params)
 
-    render json: {
-      message: "success",
-      data: @article
-    }, status: :ok
-  end
+        render json: {
+          message: 'success',
+          data: @article
+        }, status: :ok
+      end
 
-  def destroy
-    @service.deleteArticle(params[:id])
-    render json: {
-      message: "success"
-    }, status: :ok
-  end
+      def destroy
+        @service.deleteArticle(params[:id])
+        render json: {
+          message: 'success'
+        }, status: :ok
+      end
 
-  private
+      private
 
-  def service
-    @service ||= ArticleService.new
-  end
-  def article_params
-    params.permit(:title, :content)
+      def service
+        @service ||= ArticleService.new
+      end
+
+      def article_params
+        params.permit(:title, :content)
+      end
+    end
   end
 end
